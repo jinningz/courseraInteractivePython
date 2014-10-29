7ad2b5368f89c8e2db123b5b334d90bb34e27878
James <jinningz@gmail.com>
2014-10-28 21:50:08 -0400

    Upload project 5

---
+++ Project5_Memory.py
@@ -0,0 +1,86 @@
+# implementation of card game - Memory
+
+import simplegui
+import random
+
+# helper function to initialize globals
+def new_game():
+    global deck, exposed, state, numTurns, memory
+    
+    # Initiate deck and shuffle
+    deck = range(8) + range(8) 
+    random.shuffle(deck)
+    
+    # The following list stores whether a card is exposed
+    exposed = [False]*16
+    
+    # The following variable stores the state of clicks
+    state = 0
+    
+    numTurns = 0
+    
+    # The following variable is used to compare exposed cards
+    # Squence of card position clicked
+    memory = []
+ 
+    
+# define event handlers
+def mouseclick(pos):
+    global exposed, state, numTurns, memory
+    
+    #print pos
+    cardClick = pos[0] // 50
+    
+    if exposed[cardClick] == False:
+        exposed[cardClick] = True
+        if state == 0:
+            state = 1
+            memory.append(cardClick)
+        elif state == 1:
+            state = 2
+            numTurns += 1
+            memory.append(cardClick)
+        else:
+            state = 1
+            memory.append(cardClick)
+            if deck[memory[-2]] <> deck[memory[-3]]:
+                exposed[memory[-2]] = False
+                exposed[memory[-3]] = False      
+    
+    
+# cards are logically 50x100 pixels in size    
+def draw(canvas):
+    for i in range(len(deck)):
+
+        # Draw cards
+        if exposed[i] == True:
+            canvas.draw_text(str(deck[i]), (50*(i)+20, 65), 20, "White")
+        else:
+            canvas.draw_polygon([(50*(i), 0), (50*(i), 100),
+                                 (50*(i+1), 100), (50*(i+1), 0)],
+                                 1, "Green", "Green")
+        
+        # Draw borders
+        if i <> 0:
+            canvas.draw_line((50*i, 0), (50*i, 100), 1, "White")            
+        
+        label.set_text("Turns = " + str(numTurns))
+        label2.set_text("state: card " + str(state))
+
+        
+# create frame and add a button and labels
+frame = simplegui.create_frame("Memory", 800, 100)
+frame.add_button("Reset", new_game)
+label = frame.add_label("Turns = 0")
+label2 = frame.add_label("State: card 0")
+
+# register event handlers
+frame.set_mouseclick_handler(mouseclick)
+frame.set_draw_handler(draw)
+
+# get things rolling
+new_game()
+frame.start()
+
+
+# Always remember to review the grading rubric
--- README.md
+++ README.md
@@ -8,29 +8,34 @@ Click the links below to play these games on your favourite browser!
 Click the play button to start.  
   
 
-**Project 1**
-Rock Paper Scissors Lizard Spock
+**Project 1**  
+Rock Paper Scissors Lizard Spock  
 
 http://www.codeskulptor.org/#user38_kk8JKYrbYu2wmcd.py
   
   
-**Project 2**
-Guess the number game
+**Project 2**  
+Guess the number game  
 
 http://www.codeskulptor.org/#user38_jO2LAAV5wJuYei8.py
   
   
-**Project 3**
-Stopwatch
+**Project 3**   
+Stopwatch  
 
 http://www.codeskulptor.org/#user38_UmGtP4vZKU96N6J.py
   
   
-**Project 4**
-Pong
+**Project 4**  
+Pong  
 *(use w/s and up/down key on keyboard to control paddles)*
 
 http://www.codeskulptor.org/#user38_hjIuktiRiRMUqZe.py
+
+**Project 5**  
+Memory  
+
+http://www.codeskulptor.org/#user38_V1hKJzT7aAJGN4p.py
   
   
 
